900d4e5b85ae6dc31ee9acb1633fe855a4d21861
fuad-alii <fuad.c186@gmail.com>
2020-02-25 22:03:04 +0700

    add front and requirements

--- fuadali/my_app/views.py
+++ fuadali/my_app/views.py
@@ -1,5 +1,16 @@
+import request
 from django.shortcuts import render
+from bs4 import Beatifulsoup
+
 
 # Create your views here.
 def home(request):
     return render(request,'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff = {'search':search,}
+    return render (request,'my_app/new_search.html',stuff  )
+
+def tespage(request):
+    return render (request,'my_app/tespage.html')
